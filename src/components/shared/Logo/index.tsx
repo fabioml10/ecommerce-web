45d6d7e80a751cd1b833684d3a4a257dc12370f6
Fabio Muller <fabioml10@gmail.com>
2021-12-24 09:53:36 -0300

    Add header

---
+++ src/components/shared/Logo/index.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Logo: React.FC = () => {
+	return (
+    <Link href="/">
+			<a>
+				<Image
+					src="/logo-games.png"
+					alt="Logo OneBitGames"
+					width={220}
+					height={40}
+				/>
+			</a>
+    </Link>
+	)
+}
+
+export default Logo
---
+++ src/components/shared/MainComponent/index.tsx
@@ -0,0 +1,16 @@
+
+import Header from '../Header/StorefrontHeader'
+
+const MainComponent: React.FC = ({children}) => {
+	return (
+		<div className="d-flex flex-column sticky-footer-wrapper">
+			<Header />
+			<div className="container flex-fill">
+				{children}
+			</div>
+			Footer
+		</div>
+	)
+}
+
+export default MainComponent
