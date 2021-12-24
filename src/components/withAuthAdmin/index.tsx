a9b098edba79c71b9979489b3ae838f1df874cea
Fabio Muller <fabioml10@gmail.com>
2021-12-24 10:42:04 -0300

    Add api call and HOC for admin

---
+++ src/components/withAuthAdmin/index.tsx
@@ -0,0 +1,39 @@
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/router'
+import Cookie from 'js-cookie'
+import AuthState from '../../dtos/AuthState'
+import User from '../../dtos/User'
+import ApiData from '../../dtos/ApiData'
+
+const withAuthAdmin = (Component) => {
+  const Auth = (props) => {
+    const router = useRouter()
+    const loggedUser: User = useSelector((state: AuthState) => state.auth.loggedUser)
+    const apiDataCookie = Cookie.get('@api-data')
+    const apiData: ApiData = apiDataCookie ? JSON.parse(apiDataCookie) : null
+
+    if (!loggedUser || 
+        loggedUser.profile !== 'admin' ||
+        !apiData ||
+        !apiData['access-token'] ||
+        apiData['aceess-token'] === ''
+    ) {
+      router.push({
+        pathname: '/Auth/Login',
+        query: {
+          callback: router.pathname
+        }
+      })
+    }
+
+    return <Component {...props} />
+  }
+
+  if(Component.getServerSideProps) {
+    Auth.getServerSideProps = Component.getServerSideProps
+  }
+
+  return Auth
+}
+
+export default withAuthAdmin
