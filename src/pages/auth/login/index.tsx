b88475698602af3f226d93322f076d076486ec85
Fabio Muller <fabioml10@gmail.com>
2021-12-24 10:52:24 -0300

    Add signin and signup forms

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,12 +1,14 @@
 import MainComponent from "../../../components/shared/MainComponent"
 import LoginForm from "../../../components/LoginForm"
+import SignUpForm from "../../../components/SignUpForm"
 
 const Login: React.FC = () => {
   return (
     <MainComponent>
       <div className="p-4 text-center">
-        <h2>Entrar</h2>
         <LoginForm titlePhrase="Acessar minha conta" buttonPhrase="ACESSAR" />
+        <br />
+        <SignUpForm titlePhrase="Criar nova conta" buttonPhrase="CRIAR" />
       </div>
     </MainComponent>
   )
