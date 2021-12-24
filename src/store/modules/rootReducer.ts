b5f42cfda9b973ccc22ce127426ea80a13463b80
Fabio Muller <fabioml10@gmail.com>
2021-12-24 11:45:23 -0300

    Refactor seach

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import auth from './auth/reducer'
+import search from './admin/shared/search/reducer'
 
 export default combineReducers({
-  auth
+  auth,
+  search
 })
