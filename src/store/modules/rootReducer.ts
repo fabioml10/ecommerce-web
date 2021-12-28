1ea2efef3e0f19cc2b83603363e0f13165801d3f
Fabio Muller <fabioml10@gmail.com>
2021-12-28 11:08:28 -0300

    Add products list

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import auth from './auth/reducer'
 import search from './admin/shared/search/reducer'
 import category from './admin/category/reducer'
+import product from './admin/product/reducer'
 
 export default combineReducers({
   auth,
   search,
-  category
+  category,
+  product
 })
