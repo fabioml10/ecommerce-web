0140fc03ffb8affcfa48f116c4fdf8f14524bc09
Fabio Muller <fabioml10@gmail.com>
2021-12-30 15:12:18 -0300

    Add complete search function

--- src/components/shared/Header/StorefrontHeader/index.tsx
+++ src/components/shared/Header/StorefrontHeader/index.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 import { InputGroup, FormControl, Row, Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faShoppingCart, faUserCircle } from '@fortawesome/free-solid-svg-icons'
 import Logo from '../../Logo'
-import Link from 'next/link';
+import Link from 'next/link'
+import ProductSearchService from '../../../../util/ProductSearchService'
 import styles from './styles.module.css'
 
 const StorefrontHeader: React.FC = () => {
+  const [search, setSearch] = useState('')
+  const router = useRouter()
+
+  const handleSearch = (): void => {
+    router.push(`
+      /search${ProductSearchService.execute({ search })}
+    `)
+  }
+
   return (
     <Row className={styles.background}>
       <Col md={6} className="mt-2">
@@ -14,20 +26,41 @@ const StorefrontHeader: React.FC = () => {
       <Col md={6} className="mt-2 text-center">
         <Row>
           <Col md={6} className="mb-4 mb-md-0">
-            <InputGroup>
-              <FormControl placeholder="Pesquisar produto" className={styles.input} />
+          <InputGroup 
+            className={`${router.pathname === '/search' ? styles.hidden: ''}`}>
+            <FormControl 
+              placeholder="Pesquisar produto" 
+              value={search}
+              onChange={
+                (evt: React.ChangeEvent<HTMLInputElement>) =>
+                  setSearch(evt.target.value)
+              }
+
+              onKeyUp={
+                (evt: React.KeyboardEvent<HTMLInputElement>) => {
+                  if (evt.key.toLowerCase() === 'enter') {
+                    handleSearch();
+                  }
+                }
+              }
+            />
             </InputGroup>
           </Col>
           <Col md={6}>
             <Row>
-              <Col md={4} xs={4}>
-                <FontAwesomeIcon icon={faSearch} color="var(--color-gray-light)" />
+              <Col 
+                className={`${router.pathname === '/search' ? styles.hidden: ''}`}>
+                <FontAwesomeIcon 
+                  icon={faSearch} 
+                  color="var(--color-gray-light)" 
+                  onClick={handleSearch}
+                />
               </Col>
               <Col md={4} xs={4}>
                 <FontAwesomeIcon icon={faShoppingCart} color="var(--color-gray-light)" />
               </Col>
               <Col md={4} xs={4}>
-              <Link href="/Auth/Login">
+              <Link href="/auth/login">
                 <a>
                   <FontAwesomeIcon icon={faUserCircle} color="var(--color-gray-light)" />
                 </a>
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,227 @@
+import { useState, useEffect } from 'react'
+import MainComponent from '../../components/shared/MainComponent'
+import { InputGroup, FormControl, Row, Col } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import ProductInfo from '../../components/shared/ProductInfo'
+import useSwr from 'swr'
+import { useRouter } from 'next/router'
+import SearchService from '../../services/search'
+import ProductSearchService from '../../util/ProductSearchService'
+import CategoriesService from '../../services/categories'
+import { toast } from 'react-toastify'
+import styles from './styles.module.css'
+
+const defaultUrl = '/storefront/v1/products'
+
+const Search: React.FC = () => {
+  const router = useRouter()
+  const { 
+    search: searchRouter, 
+    page, 
+    category, 
+    price, 
+    order: orderRouter, 
+    direction 
+  } = router.query
+
+  const [search, setSearch] = useState(searchRouter?.toString())
+  const [order, setOrder] = useState(() => {
+    if (!!orderRouter) {
+      return `${orderRouter.toString()}-${router.query.direction.toString()}`
+    }
+
+    return 'price-asc'
+  })
+
+  const [url, setUrl] = useState(() => (
+    defaultUrl +
+    ProductSearchService.execute({
+      search,
+      order: orderRouter,
+      direction
+    })
+  ))
+
+  const { data, error } = useSwr(url, SearchService.execute)
+
+  const { data: categoriesData, error: categoriesError } = 
+    useSwr('/storefront/v1/categories?length=999', CategoriesService.index)
+
+    useEffect(() => {
+      setUrl (
+        defaultUrl +
+        ProductSearchService.execute({
+          search: searchRouter,
+          page,
+          category,
+          price,
+          order: orderRouter,
+          direction
+        })
+      )
+    }, [searchRouter, page, category, price, orderRouter, direction])
+
+    useEffect(() => {
+      router.push({
+        pathname: router.pathname,
+        query: {
+          ...router.query,
+          page: 1,
+          order: order.split('-')[0],
+          direction: order.split('-')[1]
+        }
+      })
+    }, [order])
+
+    const handleSearch = (): void => {
+      router.push(`
+        /search${ProductSearchService.execute({ search })}
+      `)
+    }
+  
+    if (error) {
+      toast.error('Erro ao pesquisar produtos.')
+    }
+  
+    if (categoriesError) {
+      toast.error('Error ao obter as categorias.')
+    }
+
+
+  return(
+    <MainComponent>
+      <div>
+        <div className="text-center mt-4">
+          <h3 className={styles.title}>
+            Resultados da Pesquisa
+          </h3>
+        </div>
+
+        <Row className="text-center col-md-6 offset-md-3">
+          <Col xs={10}>
+            <InputGroup>
+              <FormControl 
+                placeholder="Pesquisar" 
+                value={search}
+                onChange={
+                  (evt: React.ChangeEvent<HTMLInputElement>) => 
+                    setSearch(evt.target.value)
+                }
+
+                onKeyUp={
+                  (evt: React.KeyboardEvent<HTMLInputElement>) => {
+                    if (evt.key.toLowerCase() === 'enter') {
+                      handleSearch();
+                    }
+                  }
+                }
+              />
+            </InputGroup>
+          </Col>
+
+          <Col xs={2}>
+            <FontAwesomeIcon 
+              icon={faSearch} 
+              size="2x" 
+              className={styles.search_icon}
+              onClick={handleSearch}
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm={6} xs={12} className={styles.results}>
+            <span>
+              {data?.meta?.total} resultado(s)
+            </span>
+          </Col>
+
+          <Col sm={6} xs={12}>
+            <div className={styles.ordenation}>
+              <strong className="mr-3">Ordenar por:</strong>
+              <select 
+                className={styles.secondary}
+                value={order}
+                onChange={
+                  (evt: React.ChangeEvent<HTMLSelectElement>) => 
+                    setOrder(evt.target.value)
+                }
+              >
+                <option value="price-asc">Menor preço</option>
+                <option value="price-desc">Maior preço</option>
+                <option value="release_date-desc">Lançamentos</option>
+                <option value="release_date-asc">Mais antigos</option>
+              </select>
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <select 
+            className={styles.primary}
+            onChange= {
+              (evt: React.ChangeEvent<HTMLSelectElement>) =>
+                router.push({
+                  pathname: router.pathname,
+                  query: {
+                    ...router.query,
+                    page: 1,
+                    category: evt.target.value
+                  }
+                })
+            }
+          >
+            <option value="">Categoria</option>
+            {
+              categoriesData?.categories?.map(
+                category => (
+                  <option 
+                    value={category.id}
+                    key={category.id}
+                  >
+                    {category.name}
+                  </option>
+                )
+              )
+            }
+          </select>
+
+          <select className={styles.primary}
+            onChange={
+              (evt: React.ChangeEvent<HTMLSelectElement>) => 
+                router.push({
+                  pathname: router.pathname,
+                  query: {
+                    ...router.query,
+                    page: 1,
+                    price: evt.target.value
+                  }
+                })
+            }
+          >
+            <option value="">Preço</option>
+            <option value="0-50">0 - 50</option>
+            <option value="50.01-100">50 - 100</option>
+            <option value="100.01-150">100 - 150</option>
+            <option value="150.01">+ 150</option>
+            </select>
+        </Row>
+
+        <Row className="mt-4 mb-4">
+          {
+            data?.products?.map(
+              product => (
+                <Col md={3} key={product.id}>
+                  <ProductInfo product={product}/>
+                </Col>
+              )
+            )
+          }
+        </Row>
+      </div>
+    </MainComponent>
+  )
+}
+
+export default Search
