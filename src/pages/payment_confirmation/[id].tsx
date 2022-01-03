596d3f8908b3de8206e75f95cd9cc54a60b3e181
Fabio Muller <fabioml10@gmail.com>
2022-01-03 12:37:22 -0300

    Add order details

--- src/pages/payment_confirmation/[id].tsx
+++ src/pages/payment_confirmation/[id].tsx
@@ -1,35 +1,25 @@
 import MainComponent from '../../components/shared/MainComponent'
 import StyledButton from '../../components/shared/StyledButton'
-
-import styles from './styles.module.css'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faGamepad, faCheckSquare } from '@fortawesome/free-solid-svg-icons'
-
-import BlueBackground from '../../components/shared/BlueBackground'
-
-import { Col, Row, Badge } from 'react-bootstrap'
-
+import { Col, Row } from 'react-bootstrap'
 import withAuth from '../../components/withAuth'
-import useSwr from 'swr';
-import { useRouter } from 'next/router';
-
-import OrderService from '../../services/order';
-import { toast } from 'react-toastify';
+import useSwr from 'swr'
+import { useRouter } from 'next/router'
+import OrderService from '../../services/order'
+import { toast } from 'react-toastify'
+import OrderDetail from '../../components/shared/OrderDetail'
+import styles from './styles.module.css'
 
 const PaymentConfirmation: React.FC = () => {
-  const router = useRouter();
-
-  const { id } = router.query;
+  const router = useRouter()
+  const { id } = router.query
 
   const { data, error } = useSwr(
     () => id ? `/storefront/v1/orders/${id}` : null, OrderService.show
-  );
+  )
 
-  if (error) {
-    toast.error('Erro ao obter os dados do pedido!');
-    console.log(error);
-  }
+  if (error) toast.error('Erro ao obter os dados do pedido!')
 
   return (
     <MainComponent>
@@ -56,61 +46,7 @@ const PaymentConfirmation: React.FC = () => {
         </div>
       </div>
 
-      <BlueBackground>
-        <strong>PRODUTO</strong>
-        <strong className="float-right">VALOR</strong>
-
-        {
-          data?.line_items?.map(
-            (item, index) => (
-              <div key={index}>
-                <hr className={styles.line} />
-                <Row className={styles.product}>
-                  <Col 
-                    sm={3} 
-                    xs={12} 
-                    className="text-center"
-                  >
-                    <img 
-                      src={item?.image_url}
-                      alt={item?.product}            
-                    />
-                  </Col>
-
-                  <Col sm={7} xs={9}>
-                    <strong 
-                      className={styles.product_name}
-                    >
-                      {item?.product}
-                    </strong>
-
-                    <div>
-                      {
-                        item?.categories?.map(
-                          (category, index) =>
-                            <Badge 
-                              key={index}
-                              bg="primary" 
-                              className={styles.primary_badge}
-                            >
-                              {category}
-                            </Badge>
-                        )
-                      }
-                    </div>
-                  </Col>
-
-                  <Col sm={2} xs={3} className="text-center">
-                    <strong className={styles.price}>
-                      {item?.payed_price}
-                    </strong>
-                  </Col>
-                </Row>
-              </div>
-            )
-          )
-        }
-      </BlueBackground>
+      <OrderDetail items={data?.line_items}/>
 
       <Row className="mt-4 mb-4">
         <Col>
